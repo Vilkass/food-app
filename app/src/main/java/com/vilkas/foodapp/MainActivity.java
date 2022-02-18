b1586a31f6e8543d237238338f6ea8e1c0ebd0d5
Vilkass <antankas@gmail.com>
2022-02-18 20:39:00 +0200

    New activities
    - Added login, register activities layout\n- Added MainActivity button events to navigate to login/register activities

--- app/src/main/java/com/vilkas/foodapp/MainActivity.java
+++ app/src/main/java/com/vilkas/foodapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.vilkas.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,17 +19,17 @@ public class MainActivity extends AppCompatActivity {
 
     // Opens log in window
     public void logInWindow(View view){
-
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
 
     // Opens register window
     public void registerWindow(View view){
-
+        startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
     }
 
     // User continues as guest
     public void guestWindow(View view){
-
+        startActivity(new Intent(getApplicationContext(), SearchActivity.class));
     }
 
 }
